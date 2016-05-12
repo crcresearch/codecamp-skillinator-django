7aad40bc72f1c1a739315fd4a44761b7089a0a32
Elizabeth Caldwell <ecaldwe1@nd.edu>
2016-05-12 15:54:55 -0400

    Django Code Camp Homework Writing Tests

--- skillsmatrix/tests.py
+++ skillsmatrix/tests.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import *
 from skillsmatrix.models import *
 import json
 from bs4 import BeautifulSoup
+import response_tests
 
 # FUNCTIONS TO TEST
 from skillsmatrix.views import SearchDeveloperSkill
+from homework import ProblemOne, ProblemTwo, ProblemThree
 
 class SkillsMatrix(TestCase):
     def setUp(self):
@@ -120,10 +122,127 @@ class SkillsMatrix(TestCase):
 
         # call the home page URL
         response = client.get('/homepage/', **{'HTTP_USER_AGENT': 'Firefox'})
-        soup = BeautifulSoup(response.content, 'lxml')
+        soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEquals(('dev1' in soup.find('h2', {'id': 'username'})), True)
 
         # check again, using IE
         response = client.get('/homepage/', **{'HTTP_USER_AGENT': 'MSIE'})
         self.assertEquals('IE not supported', response.content)
+
+class HomeworkClass(TestCase):
+    def setUp(self):
+        # Create some users
+        user1 = User.objects.create(username='neo', first_name='Keanu', last_name='Reeves')
+        user1.set_password('matrix')
+        user1.save()
+        dev1 = Developer.objects.create(user=user1, manager='Morpheus', title='Programmer')
+        dev1.save()
+
+        user2 = User.objects.create(username='beth', first_name='Beth', last_name='Caldwell')
+        user2.set_password('bethbeth')
+        user2.save()
+        dev2 = Developer.objects.create(user=user2, manager='Dave', title='Programmer')
+        dev2.save()
+
+        user3 = User.objects.create(username='jared', first_name='Jared', last_name='Olson')
+        user3.set_password('sweden')
+        user3.save()
+        dev3 = Developer.objects.create(user=user3, manager='Caleb', title='Associate Programmer')
+        dev3.save()
+
+        user4 = User.objects.create(username='trinity', first_name='', last_name='')
+        user4.set_password('trinity')
+        user4.save()
+        dev4 = Developer.objects.create(user=user4, manager='Morpheus', title='Programmer')
+        dev4.save()
+
+        self.factory = RequestFactory()
+
+    def test_Problem1_postName(self):
+        request = self.factory.post('/problem-one/', {'name': 'Beth'})
+        response = ProblemOne(request)
+        print "RESPONSE: ", response.content
+        self.assertEqual(json.loads(response.content)["name"], "Beth")
+
+    def test_Problem1_postNoName(self):
+        request = self.factory.post('/problem-one/', {'location': 'The Matrix'})
+        response = ProblemOne(request)
+        print "RESPONSE: ", response.content
+        self.assertEqual(json.loads(response.content)['name'], None)
+
+    def test_Problem1_get(self):
+        request = self.factory.get('/problem-one/', {'name': 'Beth'})
+        response = ProblemOne(request)
+        print "RESPONSE: ", response.content
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Beth')
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Caldwell')
+        self.assertEqual(json.loads(response.content)[0]['location'], 'CRC')
+
+    def test_Problem1_getNeo(self):
+        request = self.factory.get('/problem-one/', {'name': 'Keanu'})
+        response = ProblemOne(request)
+        print "RESPONSE: ", response.content
+        self.assertEqual(json.loads(response.content)[0]['first_name'], 'Keanu')
+        self.assertEqual(json.loads(response.content)[0]['last_name'], 'Reeves')
+        self.assertEqual(json.loads(response.content)[0]['location'], 'The Matrix')
+
+    def test_Problem2_IE(self):
+        client = Client()
+
+        client.login(username='neo', password='matrix')
+
+        # call the home page URL
+
+        response = client.get('/problemtwo/', **{'HTTP_USER_AGENT': 'MSIE'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual("Neo wouldn't use Internet Explorer silly...", response.content)
+
+    def test_Problem2_Firefox(self):
+        client = Client()
+
+        client.login(username='neo', password='matrix')
+
+        response = client.get('/problemtwo/', **{'HTTP_USER_AGENT': 'Firefox'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual(response.status_code, 302)
+
+    def test_Problem2_diffUserIE(self):
+        client = Client()
+
+        client.login(username='beth', password='bethbeth')
+
+        response = client.get('/problemtwo/', **{'HTTP_USER_AGENT': 'MSIE'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual("Operator...", response.content)
+
+    def test_Problem2_diffUserFirefox(self):
+        client = Client()
+
+        client.login(username='beth', password='bethbeth')
+
+        response = client.get('/problemtwo/', **{'HTTP_USER_AGENT': 'Firefox'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual("Operator...", response.content)
+
+    def test_Problem3Name(self):
+        client = Client()
+
+        client.login(username='beth', password='bethbeth')
+
+        response = client.get('/problemthree/', **{'HTTP_USER_AGENT': 'Firefox'})
+        print "RESPONSE: ", response
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual('Beth Caldwell' in str(soup.find('span', {'id': 'name'})), True)
+
+    def test_Problem3UserName(self):
+        client = Client()
+
+        client.login(username='trinity', password='trinity')
+
+        response = client.get('/problemthree/', **{'HTTP_USER_AGENT': 'Firefox'})
+        print "RESPONSE: ", response
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual('trinity' in str(soup.find('span', {'id': 'name'})), True)
+
+
 
