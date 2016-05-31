2e844f0f3c4d30566efa13f32760cb95c11953ec
Brett <bfox6@nd.edu>
2016-05-31 10:04:32 -0400

    -Finished Views HW

--- skillsmatrix/views/developer.py
+++ skillsmatrix/views/developer.py
@@ -2,15 +2,15 @@
 
 from django.views.generic import *
 from skillsmatrix.models import *
+from django.core.urlresolvers import reverse
 
 class DeveloperList(ListView):
     template_name = 'developer_list.html'
     model = Developer
 
-
 class DeveloperListByManager(DeveloperList):
     def get_queryset(self):
-        qs = super(DeveloperList, self).get_queryset()
+        qs = super(DeveloperListByManager, self).get_queryset()
         return qs.filter(manager=self.kwargs['manager'])
 
 class DeveloperDetail(DetailView):
@@ -19,28 +19,20 @@ class DeveloperDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(DeveloperDetail, self).get_context_data(**kwargs)
-        context['skills'] = DeveloperSkill.objects.filter(developer_id=self.kwargs['pk']).order_by('-years_of_experience')
+        context['skills'] = DeveloperSkill.objects.filter(developer=self.object).order_by('-years_of_experience')
         return context
-
 
 class DeveloperDetailMe(DeveloperDetail):
     def get_object(self):
         return Developer.objects.get(user=self.request.user)
 
-    def get_context_data(self, **kwargs):
-        context = super(DeveloperDetail, self).get_context_data(**kwargs)
-        context['skills'] = DeveloperSkill.objects.filter(developer=kwargs['object'])
-        return context
-
-
 class DeveloperUpdate(UpdateView):
     model = Developer
-    fields = ['manager']
+    fields = ['title']
     template_name = 'developer_update.html'
 
     def get_success_url(self):
-        return '/my_developer_details/'
-
+        return reverse('my_developer_details')
 
 class ExtraCreditCreateView(CreateView):
     model = ExtraCredit
@@ -51,6 +43,5 @@ class ExtraCreditCreateView(CreateView):
         form.instance.sender = Developer.objects.get(user=self.request.user)
         return super(ExtraCreditCreateView, self).form_valid(form)
 
-
     def get_success_url(self):
-        return '/my_developer_details/'
+        return reverse('my_developer_details')
